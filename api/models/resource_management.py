12aa4764b440295573661306b37c0e44036589cb
ZoomShroom <159501597+ZoomShroom@users.noreply.github.com>
2024-04-16 17:21:19 -0400

    feat: added models for menu_items.py, promotions.py, recipes.py, resource_management.py

--- api/models/resource_management.py
+++ api/models/resource_management.py
@@ -1,3 +1,4 @@
+#EliElk
 from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME
 from sqlalchemy.orm import relationship
 from datetime import datetime
@@ -8,7 +9,7 @@ class Resource(Base):
     __tablename__ = "resources"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    item = Column(String(100), unique=True, nullable=False)
+    name = Column(String(100), unique=True, nullable=False)
     amount = Column(Integer, index=True, nullable=False, server_default='0.0')
 
     recipes = relationship("Recipe", back_populates="resource")
