e733068a30e08c1a259027147bb55413a295e5f3
jgopali <jgopali2145@gmail.com>
2024-04-18 11:54:22 -0400

    Deleted unused files

--- api/models/create_tables.py
+++
@@ -1,9 +0,0 @@
-'''from sqlalchemy import create_engine
-from models import Base
-from ..dependencies.database import engine
-
-def index():
-    Base.metadata.create_all(engine)
-
-if __name__ == '__main__':
-    index()'''
