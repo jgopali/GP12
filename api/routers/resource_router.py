bfa2d5b6cbd48619a543da089be730d768fa1fa3
drewsecker <drewsecker1@gmail.com>
2024-04-27 18:54:18 -0400

    fix: Fixed the resources router
    Fixed variable names in the router methods that were causing errors.

--- api/routers/resource_router.py
+++ api/routers/resource_router.py
@@ -2,44 +2,44 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
 from ..schemas import resources as schemas
-from ..controllers import menu_item_resource as controller
+from ..controllers import resources as controller
 from ..dependencies.database import get_db
 
 router = APIRouter(
-    tags=['MenuItemResource'],
-    prefix="/menu_item_resource"
+    tags=['Resource'],
+    prefix="/resource"
 )
 
-# Create method for the MenuItemResource router
+# Create method for the Resource router
 @router.post("/", response_model=schemas.Resource)
-def create_menu_item_resource(menu_item_resource: schemas.ResourceCreate, db: Session = Depends(get_db)):
-    return controller.create(db=db, menu_item_resource=menu_item_resource)
+def create_resource(resources: schemas.ResourceCreate, db: Session = Depends(get_db)):
+    return controller.create(db=db, resources=resources)
 
-# Read all method for the MenuItemResource router
+# Read all method for the Resource router
 @router.get("/", response_model=List[schemas.Resource])
-def read_all_menu_item_resources(db: Session = Depends(get_db)):
+def read_all_resources(db: Session = Depends(get_db)):
     return controller.read_all(db)
 
-# Read one method for the MenuItemResource router
-@router.get("/{menu_item_resource_id}", response_model=schemas.Resource)
-def read_one_menu_item_resource(menu_item_resource_id: int, db: Session = Depends(get_db)):
-    menu_item_resource = controller.read_one(db, menu_item_resource_id=menu_item_resource_id)
-    if menu_item_resource is None:
-        raise HTTPException(status_code=404, detail="Menu Item Resource not found")
-    return menu_item_resource
+# Read one method for the Resource router
+@router.get("/{resources_id}", response_model=schemas.Resource)
+def read_one_resource(resources_id: int, db: Session = Depends(get_db)):
+    resources = controller.read_one(db, resources_id=resources_id)
+    if resources is None:
+        raise HTTPException(status_code=404, detail="Resource not found")
+    return resources
 
-# Update method for the MenuItemResource router
-@router.put("/{menu_item_resource_id}", response_model=schemas.Resource)
-def update_menu_item_resource(menu_item_resource_id: int, menu_item_resource: schemas.ResourceBase, db: Session = Depends(get_db)):
-    db_menu_item_resource = controller.read_one(db, menu_item_resource_id=menu_item_resource_id)
-    if db_menu_item_resource is None:
-        raise HTTPException(status_code=404, detail="Menu Item Resource not found")
-    return controller.update(db=db, menu_item_resource_id=menu_item_resource_id, menu_item_resource=menu_item_resource)
+# Update method for the Resource router
+@router.put("/{resources_id}", response_model=schemas.Resource)
+def update_resource(resources_id: int, resources: schemas.ResourceBase, db: Session = Depends(get_db)):
+    db_resource = controller.read_one(db, resources_id=resources_id)
+    if db_resource is None:
+        raise HTTPException(status_code=404, detail="Resource not found")
+    return controller.update(db=db, resources_id=resources_id, resources=resources)
 
-# Delete method for the MenuItemResource router
-@router.delete("/{menu_item_resource_id}")
-def delete_menu_item_resource(menu_item_resource_id: int, db: Session = Depends(get_db)):
-    db_menu_item_resource = controller.read_one(db, menu_item_resource_id=menu_item_resource_id)
-    if db_menu_item_resource is None:
-        raise HTTPException(status_code=404, detail="Menu Item Resource not found")
-    return controller.delete(db=db, menu_item_resource_id=menu_item_resource_id)
+# Delete method for the Resource router
+@router.delete("/{resources_id}")
+def delete_resource(resources_id: int, db: Session = Depends(get_db)):
+    db_resource = controller.read_one(db, resources_id=resources_id)
+    if db_resource is None:
+        raise HTTPException(status_code=404, detail="Resource not found")
+    return controller.delete(db=db, resources_id=resources_id)
