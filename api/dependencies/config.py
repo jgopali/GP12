7300b56ce270c39c7cc2a3bc5fa25afba22ad3f4
jgopali <jgopali2145@gmail.com>
2024-05-02 14:52:39 -0400

    Revert "feat: test"
    This reverts commit 7fe4f11083336e1314a0fa9f03c785e11ec2b445.

--- api/dependencies/config.py
+++ api/dependencies/config.py
@@ -3,6 +3,6 @@ class conf:
     db_name = "take_a_byte"
     db_port = 3306
     db_user = "root"
-    db_password = "@Cp6013!"
+    db_password = "Declan0319"
     app_host = "localhost"
     app_port = 8000
